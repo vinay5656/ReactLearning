d6d91b224520c56cc9abb0564e352a4c6feef299
Vinay Singh Chouhan <78366114+vinay5656@users.noreply.github.com>
2023-05-07 16:25:05 +0530

    Initial commit

---
+++ public/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function List() {
+  return (
+    <div>
+      <ul>
+        <li>Running</li>
+        <li>Reading Book</li>
+        <li>Coding</li>
+      </ul>
+    </div>
+  );
+}
+export default List;
